6719ba5ee0b4e535f90b4124dada0809b7888dda
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-08-06 20:13:39 +0700

    added navbar and footer placeholder

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+    <footer className="bg-gray-200 text-gray-700 py-4 px-6 text-sm text-center">
+        <p className="mb-2">Website created by Group G, 31 August 2024.</p>
+        <p className="mb-2">© 2024 SucoMart</p>
+        <p>Lorem ipsum</p>
+    </footer>
+    )
+  }
+  
+  export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="bg-white sticky py-2 shadow-md top-0 z-10">
+        <div className="mx-auto flex items-center px-2 justify-between ">
+          <Link href="/">
+            <img src="/placeholder_logo.png" alt="SucoMart logo" className="w-24" />
+          </Link>
+          <ul className='flex flex-row gap-4'>
+            <li>
+              <Link href="/" className="text-gray-700 hover:text-blue-500">Home</Link>
+            </li>
+            <li>
+              <Link href="/product" className="text-gray-700 hover:text-blue-500">Product</Link>
+            </li>
+            <li>
+              <Link href="/market" className="text-gray-700 hover:text-blue-500">Market</Link>
+            </li>
+            <li>
+              <Link href="/about" className="text-gray-700 hover:text-blue-500">About</Link>
+            </li>
+            <li>
+              <Link href="/contact" className="text-gray-700 hover:text-blue-500">Contact</Link>
+            </li>
+            <li>
+              <Link href="/contact" className="text-gray-700 hover:text-blue-500">Cart</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+  )
+}
+
+export default Navbar
