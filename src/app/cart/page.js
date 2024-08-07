f9850e28ad6bbcae246191829f5ea88d4565ab2c
firmanasgani <firmanasgani37@gmail.com>
2024-08-07 20:39:36 +0700

    first commit on another branch

---
+++ src/app/cart/page.js
@@ -0,0 +1,5 @@
+export default function Cart() {
+    return(
+        <h1>this is cart pages</h1>
+    )
+}
---
+++ src/app/market/page.js
@@ -0,0 +1,8 @@
+export default function Market() {
+
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    )
+}
