931f1e30d2ec453a19d0de07f167281eccf03eb7
firmanasgani <firmanasgani37@gmail.com>
2024-08-07 21:10:31 +0700

    adding card product example

--- src/app/market/page.js
+++ src/app/market/page.js
@@ -1,8 +1,65 @@
+import {
+    Image,
+    Stack,
+    Heading,
+    Card,
+    CardHeader,
+    ButtonGroup,
+    Button,
+    Text,
+    CardBody,
+    CardFooter,
+    Divider
+} from '@chakra-ui/react'
+
 export default function Market() {
 
     return (
         <>
-            <h1>Hello world</h1>
+            <div className='px-8'>
+                <h1>Selamat datang di market [nama market], [nama user]</h1>
+            </div>
+            <div className="flex flex-row px-8 flex-row justify-start">
+                {['Product 1', 'product 2'].map((products) => (
+                    <div className="basis-1/3 md:basis-1/4 px-4"  key={products}>
+                        <Card maxW='sm' variant="filled">
+                            <CardBody>
+                                <Image
+                                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                                    alt='Green double couch with wooden legs'
+                                    borderRadius='lg'
+                                />
+                                <Stack mt='6' spacing='3'>
+                                    <Heading size='md'>{products}</Heading>
+                                    <Text>
+                                        This sofa is perfect for modern tropical spaces, baroque inspired
+                                        spaces, earthy toned spaces and for people who love a chic design with a
+                                        sprinkle of vintage design.
+                                    </Text>
+                                    <Text color='blue.600' fontSize='2xl'>
+                                        $450
+                                    </Text>
+                                </Stack>
+                            </CardBody>
+                            <Divider />
+                            <CardFooter>
+                                <ButtonGroup spacing='2'>
+                                    <Button variant='solid' colorScheme='blue'>
+                                        Buy now
+                                    </Button>
+                                    <Button variant='ghost' colorScheme='blue'>
+                                        Add to cart
+                                    </Button>
+                                </ButtonGroup>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                ))}
+
+
+            </div>
+
+
         </>
     )
 }
