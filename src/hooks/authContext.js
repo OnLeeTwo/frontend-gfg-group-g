7479a04fd62d335684d7d79bb1bb899d4c568196
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-08-15 19:11:40 +0700

    modified authContext for SSR handling

--- src/hooks/authContext.js
+++ src/hooks/authContext.js
@@ -1,6 +1,27 @@
 "use client";
 
 import React, { createContext, useState, useContext, useEffect } from "react";
+import axios from "axios";
+
+const refreshToken = async () => {
+  try {
+    const refreshToken = localStorage.getItem("refresh_token");
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/refresh`,
+      { refresh_token: refreshToken }
+    );
+
+    if (response.data.access_token) {
+      localStorage.setItem("access_token", response.data.access_token);
+      return response.data.access_token;
+    } else {
+      throw new Error("No access token received");
+    }
+  } catch (error) {
+    console.error("Error refreshing token:", error);
+    return null;
+  }
+};
 
 const AuthContext = createContext();
 
@@ -8,8 +29,18 @@ export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem("access_token");
-    setIsLoggedIn(!!token);
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem("access_token");
+      setIsLoggedIn(!!token);
+    };
+
+    checkLoginStatus();
+
+    const refreshInterval = setInterval(async () => {
+      await refreshToken();
+    }, 59 * 60 * 1000); // 59 minutes
+
+    return () => clearInterval(refreshInterval);
   }, []);
 
   return (
