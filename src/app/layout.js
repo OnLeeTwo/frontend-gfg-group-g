f01d7eb73a01010dc2727b9d00802fb19b6219bd
Richo Wardiyanto <richowardiyanto1@gmail.com>
2024-08-19 22:27:14 +0700

    add path @/

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 // import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "../hooks/authContext";
