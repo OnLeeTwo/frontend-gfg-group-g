1011fdf8c6b522ee11d359274d3876219ff0e5d0
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-08-23 16:28:29 +0700

    attemp to create new promotion manager for market

--- src/app/seller/promotions/[promotionId]/page.tsx
+++ src/app/seller/promotions/[promotionId]/page.tsx
@@ -4,28 +4,27 @@ import { ProductForm } from '../../../../components/forms/products-form';
 import { ScrollArea } from '../../../../components/ui/scroll-area';
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'next/navigation';
+import exp from 'constants';
 import withAuth from '@/middleware/withAuth';
 
-interface Product {
-  id: string;
+interface Promotion {
+  promotion_id: string;
   market_id: string;
-  product_name: string;
-  stock: number;
-  price : number;
-  description : string;
-  category : string;
-  is_premium : number;
+  code: string;
+  discount_value: number;
+  start_date : string;
+  end_date : string;
 }
 
 const ProductPageForm = () => {
-  const [initialData, setInitialData] = useState<Product | null>(null);
+  const [initialData, setInitialData] = useState<Promotion | null>(null);
   const [loading, setLoading] = useState(true);
   const params = useParams();
 
   useEffect(() => {
-  const fetchProductData = async () => {
+  const fetchPromotionData = async () => {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product/${params.productId}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/promotions`, {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${localStorage.getItem('access_token')}`,
@@ -34,7 +33,8 @@ const ProductPageForm = () => {
 
       if (response.ok) {
         const data = await response.json();
-        setInitialData(data.data[0]);
+        const filteredPromotions = data.filter(promotion => promotion.promotion_id === params.promotionId);
+        setInitialData(filteredPromotions);
       } else {
         console.error('Failed to fetch market data');
       }
@@ -45,8 +45,8 @@ const ProductPageForm = () => {
     }
   };
 
-  fetchProductData();
-}, [params.productId]);
+  fetchPromotionData();
+}, [params.promotionId]);
 
 if (loading) {
   return <div>Loading...</div>; 
@@ -55,7 +55,7 @@ if (loading) {
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-8">
-        <ProductForm
+        <PromotionForm
           initialData={initialData}
           key={initialData?.id || 'new' }
         />
---
+++ src/components/forms/promotion-form.tsx
@@ -0,0 +1,291 @@
+'use client';
+
+import * as z from 'zod';
+import { useState, useEffect } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '../ui/form';
+import { Separator } from '../ui/separator';
+import { Heading } from '../ui/heading';
+import { useToast } from '../ui/use-toast';
+import React from 'react';
+
+
+const formSchema = z.object({
+  code: z
+    .string()
+    .min(3, { message: 'Code must be at least 3 characters' }),
+  description: z
+    .string()
+    .min(3, { message: 'Product description must be at least 3 characters' }),
+  price: z.coerce.number(),
+  market_id : z.string().min(1, { message: 'Please select a market' }),
+  images: z.any().optional(),
+  stock: z.coerce.number(),
+  category: z.string().min(1, { message: 'Please select a category' }),
+  is_premium: z.boolean(),
+});
+
+type PromotionFormValues = z.infer<typeof formSchema>;
+
+interface PromotionFormProps {
+  initialData: any | null;
+  markets: { market_id: string; market_name: string }[] | null;
+}
+
+export const PromotionForm: React.FC<PromotionFormProps> = ({
+  initialData, markets
+}) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [loading, setLoading] = useState(false);
+  const [token, setToken] = useState<string | null>(null);
+
+  const title = initialData ? 'Edit promotion' : 'Create promotion';
+  const description = initialData ? 'Edit a promotion.' : 'Add new promotion';
+  const toastMessage = initialData ? 'Promotion updated.' : 'Promotion created.';
+  const action = initialData ? 'Save changes' : 'Create';
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem("access_token");
+      setToken(token);
+    }
+  }, []);
+  
+  const defaultValues = initialData || {
+        id: '',
+        product_name: '',
+        market_id: '',
+        description: '',
+        price: 0,
+        stock: 0,
+        images: [],
+        category: '',
+        is_premium: 0
+      };
+
+  const form = useForm<PromotionFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues
+  });
+
+  const onSubmit = async (data: PromotionFormValues) => {
+    try {
+      setLoading(true);
+  
+      const isPremiumValue = data.is_premium ? 1 : 0;
+  
+      const formData = new FormData();
+      formData.append("product_name", data.product_name);
+      formData.append("description", data.description);
+      formData.append("market_id", data.market_id);
+      formData.append("price", data.price.toString());
+      formData.append("stock", data.stock.toString());
+      formData.append("category", data.category);
+      formData.append("is_premium", isPremiumValue.toString());
+  
+      if (data.images) {
+        formData.append("images", data.images);
+      }
+  
+      const response = initialData
+        ? await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product/${initialData.id}`, {
+            method: "PUT",
+            headers: { Authorization: `Bearer ${token}` },
+            body: formData,
+          })
+        : await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product`, {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+            body: formData,
+          });
+  
+      if (response.ok) {
+        toast({
+          title: toastMessage,
+          description: `Product ${initialData ? 'updated' : 'created'} successfully.`,
+          duration: 3000,
+        });
+        router.push(`/seller/products`);
+      } else {
+        throw new Error("Failed to process product");
+      }
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Uh oh! Something went wrong.',
+        description: 'There was a problem with your request.',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          {!initialData && (
+            <FormField
+            control={form.control}
+            name="market_id"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Market</FormLabel>
+                <FormControl>
+                  <select
+                    disabled={loading}
+                    {...field}
+                  >
+                    <option value="">Select a market</option>
+                    {markets?.map(market => (
+                      <option key={market.market_id} value={market.market_id}>
+                        {market.market_name}
+                      </option>
+                    ))}
+                  </select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          )}
+          <FormField
+            control={form.control}
+            name="images"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Images</FormLabel>
+                <FormControl>
+                <input
+                    type="file"
+                    accept="image/*"
+                    disabled={loading}
+                    onChange={(e) => {
+                      field.onChange(e.target.files?.[0]);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="gap-8 md:grid md:grid-cols-3">
+            <FormField
+              control={form.control}
+              name="product_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Product name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Product description"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <FormControl>
+                    <Input type="number" disabled={loading} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="stock"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Stock</FormLabel>
+                  <FormControl>
+                    <Input type="number" disabled={loading} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                    <FormControl>
+                      <Input type="string" disabled={loading} placeholder="Product category" {...field} />
+                    </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="is_premium"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>I want to list this product as a premium quality</FormLabel>
+                    <FormControl>
+                    <Input
+                      type="checkbox"
+                      disabled={loading}
+                      checked={field.value ?? false}
+                      onChange={(e) => field.onChange(e.target.checked)}
+                    />
+                    </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
