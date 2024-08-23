3677833393cd77614c2151befbe0488313c0b2ab
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-08-23 14:22:02 +0700

    removed token depedencies

--- src/hooks/productFetch.js
+++ src/hooks/productFetch.js
@@ -1,24 +1,22 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetchProduct = (id, token) => {
+const useFetchProduct = (id) => {
   const [product, setProduct] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchProduct = async () => {
-      if (!id || !token) return;
+      if (!id) {
+        setIsLoading(false);
+        return;
+      }
 
       try {
         setIsLoading(true);
         const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}/product/${id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
+          `${process.env.NEXT_PUBLIC_API_URL}/product/${id}`
         );
         if (response.data.success && response.data.data.length > 0) {
           setProduct(response.data.data[0]);
@@ -33,7 +31,7 @@ const useFetchProduct = (id, token) => {
     };
 
     fetchProduct();
-  }, [id, token]);
+  }, []);
 
   return { product, error, isLoading };
 };
